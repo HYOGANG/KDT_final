3748086f6a8fdeb60df5da0def3ccf1862007fcb
HYOGANG <gyrud569@gmail.com>
2023-11-20 14:22:49 +0900

    1120

--- finalapp/urls.py
+++ finalapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard_view, name='dashboard'),
-    path('board', views.dashboard_view, name='dashboard'),
-    path('map', views.map),
+    path('', views.dashboard_view),
+    path('board/', views.dashboard_view),
+    path('map/', views.map),
 ]
