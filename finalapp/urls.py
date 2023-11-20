4be3a6af9246603b7eab09c5bc23d34e9777be20
HYOGANG <gyrud569@gmail.com>
2023-11-20 13:47:55 +0900

    test추가

--- finalapp/urls.py
+++ finalapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard_view, name='dashboard'),
-    path('board', views.dashboard_view, name='dashboard'),
-    path('map', views.map),
+    path('board/', views.dashboard_view, name='dashboard'),
+    path('map/', views.map),
+    path('test/', views.test),
 ]
